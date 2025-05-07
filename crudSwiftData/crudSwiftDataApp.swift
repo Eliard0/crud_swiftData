aaebd2f34dc859fe3e3c618a5eb1ac992b458eae
Eliardo <eliardosilva81@gmail.com>
2025-05-07 09:54:04 -0300

    Initial Commit

---
+++ crudSwiftData/crudSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  crudSwiftDataApp.swift
+//  crudSwiftData
+//
+//  Created by Eliardo Venancio on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct crudSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
